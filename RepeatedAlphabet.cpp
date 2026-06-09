4bbdc8777128701609e48877577e7d3379beeb08
Vikash Kumar <25110cn235@glbitm.ac.in>
2026-06-09 11:19:00 +0530

    Day 9 Assignment

---
+++ RepeatedAlphabet.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+// REPEATED ALPHABET PATTERN
+
+int main(){
+    int n;
+    cout << "Enter height of pyramid : ";
+    cin >> n;
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=i;j++){
+            cout << char(64+i) << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
---
+++ ReverseNumberr.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+// REVERSE NUMBER PATTERN
+
+int main(){
+    int n;
+    cout << "Enter height of pyramid : ";
+    cin >> n;
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=(n-i+1);j++){
+            cout << j << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
